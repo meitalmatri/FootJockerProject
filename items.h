5ad9513257c6788074de161eca1004ed2ee5bffa
yohad1 <161965030+yohad1@users.noreply.github.com>
2024-03-02 19:39:29 +0200

    Update items.h

--- items.h
+++ items.h
@@ -2,12 +2,34 @@
 
 #include <stdbool.h>
 
-typedef struct item
-{
-        int id;
-        char model[20];
-        char manuf[20];
-        int size[11];
-        float price;
-        int inventory;
+typedef struct item{
+    int id;
+    char model[20];
+    char manuf[20];
+    int size[11];
+    float price;
+    int inventory;
+
 } Item;
+
+node* search(node tree, int val)
+{
+	if (!(tree))
+	{
+		return tree;
+	}
+
+	if (val < (tree)->id)
+	{
+		search(&((tree)->left), val);
+	}
+	else if (val > (tree)->id)
+	{
+		search(&((tree)->right), val);
+	}
+	else if (val == (tree)->id)
+	{
+		return tree;
+	}
+
+}
