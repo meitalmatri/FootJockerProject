ac59d7817e921a28642fcda9ac60d4d25a05f1c9
dananaf <dananaf89@gmail.com>
2024-04-29 13:14:52 +0300

    printLog

--- log.c
+++ log.c
@@ -1,3 +1,4 @@
+#include "log.h"
 #include "employees.h"
 #include "menu.h"
 #include <stdio.h>
@@ -6,40 +7,25 @@
 #include "employees.h"
 #include "dates.h"
 
-
-
-#define MAX_DATE_LENGTH 20
-#define MAX_USERNAME_LENGTH 50
-#define MAX_ACTION_LENGTH 100
-#define LOG_SIZE 500
-
-typedef struct {
-    char date[MAX_DATE_LENGTH];
-    char username[MAX_USERNAME_LENGTH];
-    char action[MAX_ACTION_LENGTH];
-    int success; 
-} LogEntry;
-
-
 void printLogEntry(const LogEntry* entry) {
     printf("%s\t%s\t%s\t%s\n", entry->date, entry->username, entry->action, (entry->success ? "Success" : "Failure"));
 }
 
 
-void writeLog(const LogEntry* entry, const char* filename) {
+void writeLog(const LogEntry* entry, const char* filename,int j) {
     FILE* logfile = fopen(filename, "a");
     if (logfile == NULL) {
         printf("Error opening log file!\n");
         return;
     }
-
+    for(int i=0;i<j;i++)
     fprintf(logfile, "%s\t%s\t%s\t%s\n", entry->date, entry->username, entry->action, (entry->success ? "Success" : "Failure"));
 
     fclose(logfile);
 }
 
 int loging()
-
+{
     LogEntry log[LOG_SIZE];
 
     time_t rawtime;
@@ -69,4 +55,13 @@ int loging()
     }
 
     return 0;
+}
+
+
+void printLog(LogEntry** log, int* i,char* username ,char* word)
+{
+    strcpy(log[*i]->date, getCurrentDate());
+    strcpy(log[*i]->username, username);
+    strcpy(log[*i]->action, word);
+    *i = *i + 1;
 }
--- log.h
+++ log.h
@@ -6,7 +6,21 @@
 #include "employees.h"
 #include "dates.h"
 #include<malloc.h>
-#include"log.c"
 
+#define MAX_DATE_LENGTH 20
+#define MAX_USERNAME_LENGTH 50
+#define MAX_ACTION_LENGTH 100
+#define LOG_SIZE 500
 
+typedef struct {
+    char date[11];
+    char username[20];
+    char action[100];
+    int success;
+} LogEntry;
+
+void printLog(LogEntry** log, int* i, char* username, char* word);
+void writeLog(const LogEntry* entry, const char* filename, int j);
+void printLogEntry(const LogEntry* entry);
+int loging();
 
